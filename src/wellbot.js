8dda0bbc6203a14e6dd3586b7514d0bfbc3bb9b5
Yann <30565751+SeenKid@users.noreply.github.com>
2022-11-15 10:20:38 +0100

    Update wellbot.js

--- src/wellbot.js
+++ src/wellbot.js
@@ -79,9 +79,6 @@ const voice = new aoijs.Voice(bot, {
 
 }); 
 
-   
-
-
 bot.joinCommand({
 
  channel: "$getServerVar[bvn]",
@@ -108,13 +105,6 @@ $suppressErrors
    }) 
 bot.onJoin()
 
-
- 
- 
-
-
-
-
 bot.leaveCommand({
 channel: "$getServerVar[leave]",
 code: `$title[1;😢 Un membres vient de partir ! 😢] 
@@ -128,72 +118,6 @@ $color[1;RANDOM]
    }) 
 
 bot.onLeave()
-
-
-bot.command({
-    name: 'anniversaire', 
-    code: `$color[1;RANDOM]
-$description[1;**🎂 JOYEUX ANNIVERSAIRE JUSTINE !!!! 🎂**
-
-Coucou Justine, Aujourd'hui c'est un jour spécial, même si tu le fête pas, on avait envie de te remercier pour tout ce que tu fais chaque jour pour nous, tes une amie et une très bonne pour certain, chaque jour t'illumine notre journée. 
-On sais tous rassembler, pour te faire un petit mot chacun en espérant que sa te plaise !
-
-**__De la part d'Arthur__ :**
-
-Je ne sais pas si tu sais toute l’importance que tu as pour moi. Je réalise combien je suis chanceux/se de te compter parmi mes meilleurs amis. Tu as toujours les mots pour me faire rire ou me consoler. Tu restes la personne dont je me sens la plus proche et à qui je peux tout dire. Je sais que tu ne me jugeras jamais et que tu chercheras toujours à m’aider qu’importe la situation. Ton amitié est précieuse et indispensable à mon bonheur.
-
-
-**__De la part de Gigi__ :**
-
-Je te souhaite un joyeux anniversaire jujuuuu profite un max de cette journée si spécial en se jour je te souhaite également que du bonheur a toi et a ta famille reste positive et je t'encourage a profiter de la vie :partying_face:
-
-
-**__De la part de Rosie et amira__ :**
-
-En ce jour spécial nous voulons te rappeler à quel point tu comptes pour nous . Depuis que nous te connaissons nous t 'adorons et te considérons comme étant notre soeur .  Nous voulons te rappeler que tu es une personne exeptionelle et que nous avons beaucoup de chance de t'avoir , dès notre rencontre nous avons vu que de qualités en toi . En si peu de temps tu es devenue notre famille et tu as pris une énorme place dans notre coeur .
-
-
-**__De la part de Wawan__ :**
-
-Joyeux anniv Juju tu reste une super pote t'es sympa et tous bref passe la meilleure journée de ta vie elle t'est dédié alors profite au max
-
-**__De la part de Sarah__ :**
-
-Joyeux anniversaire Justine, tu pourras toujours compter sur moi
-
-
-**__De la part D'oxi__ :**
-
-Bonne anniversaire maman, c'est beau tu va avoir 15 ans :clapwink:, on ne ce connais pas depuis très longtemps mais je sait que tu es une très bonne amie que tout le monde rêve d'avoir, a des moments t'es drôle mais à d'autres non tu comprends pourquoi 😂, on a rigolé ensemble comme la dernière fois où tu disais que c'était satisfaisant de donné des coups de couteau 😂 😂. Bref bonne anniversaire Juju :partying_face:
-
-Signé ton fils préféré 😏:sunglasses:
-
-
-**__De la part de loulou__ :**
-
-C’est assez rare de pouvoir être soi-même avec une autre personne qu’un membre de sa famille ou la personne qu'on aime. C’est une belle opportunité offerte de se sentir à l'aise avec une personne que je n'es jamais vus en vrai...
-
-Alors je t'es écrit ce mot pour te dire à qu'elle point tu compte pour moi tu es sans doute une des plus belles personnes que je connaisse,une de mes meilleures amies....
-
-Signaler Loulou_l'allemande 🇩🇪
-
-**Et finalement**
-**__De la part de Raph__ :**
-
-Coucou Justine,je sais que tu fetes pas ton anniversaire mais je voulais te dire ,combien tu compte a nos yeux, Je vais pas ecrire un discours,car franchement même si j'écris 10000 ligne je pourrais pas te dire combien tu comptes réellement à mes yeux,1an et 4 mois qu'on se connait(j'ai pas du tout check la date où on est devenue ami,je vois pas de quoi tu parle mdrr) et on est deja aussi proche mdr, t'es tout pour moi, si je suis énervé je sais que je peux te parler, je serais toujours là pour toi sache le, je te considère pas comme ma sœur, toi même tu sais pourquoi 😂, mais bon t'es l'a des personnes qui compte le plus pour moi. 
-
-Joyeux anniversaire ma meilleure amie^^
-
-prend soins de toi bisous je taime 😋 🎂 💗💗
-
-**Voilà en espérant que sa te plaise, de la part de tout le monde !**]
- 
-$image[1;https://cdn.discordapp.com/attachments/944192569674453042/944303437649154068/16452098731431354037987.jpg] 
-$thumbnail[1;https://cdn.discordapp.com/avatars/759357189529534485/3aad9881da8a76980ee5cb188e7c93ea.webp] 
-$addReactions[🎂;🎉;💗]
-$onlyForIDs[759357189529534485;762772346620608524;seul Justine peut la faire pas toi^^] 
-`
-   })
 
  
 voice.trackStartCommand({
@@ -222,9 +146,3 @@ $thumbnail[1;$songInfo[thumbnail]]
 })
 
 voice.onTrackStart() 
-
-   
-
-
-
-
